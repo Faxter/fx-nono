0f664325343c1c404a794b1b8f703b70c753890c
faxter <info@faxxter.com>
2025-12-08 12:15:30 +0100

    make cells clickable

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import pygame
 
-SCREEN_BACKGROUND = "black"
+SCREEN_BACKGROUND = (233, 240, 233)
 GRID_WIDTH = 5
 GRID_HEIGHT = 10
-GRID_COLOR_RGB = (200, 200, 200)
+GRID_COLOR_RGB = (20, 10, 10)
 CELL_SIZE = 50
 CELL_BORDER = 1
 GAME_FPS = 60
@@ -13,14 +13,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((GRID_WIDTH * CELL_SIZE, GRID_HEIGHT * CELL_SIZE))
     clock = pygame.time.Clock()
+    grid = [[False for _ in range(GRID_HEIGHT)] for _ in range(GRID_WIDTH)]
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
         screen.fill(SCREEN_BACKGROUND)
         for x in range(GRID_WIDTH):
             for y in range(GRID_HEIGHT):
-                pygame.draw.rect(screen, GRID_COLOR_RGB, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE), CELL_BORDER)
+                if grid[x][y]:
+                    pygame.draw.rect(screen, GRID_COLOR_RGB, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+                else:
+                    pygame.draw.rect(screen, GRID_COLOR_RGB, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE), CELL_BORDER)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.MOUSEBUTTONUP:
+                x, y = (event.pos[0] // CELL_SIZE, event.pos[1] // CELL_SIZE)
+                grid[x][y] = True
         pygame.display.flip()
         _ = clock.tick(GAME_FPS)
 
