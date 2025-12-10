79f62b269d331e10abfcf6153e856e620ef9142f
faxter <info@faxxter.com>
2025-12-10 21:14:03 +0100

    draw hints onto ui

--- src/main.py
+++ src/main.py
@@ -7,12 +7,13 @@ PUZZLE_HEIGHT = 8
 ROW_HINTS = [[3], [1, 1], [1, 1, 1], [1, 1, 1], [3], [3], [3], [1]]
 COL_HINTS = [[3], [1, 3], [1, 6], [1, 3], [3]]
 CELL_SIZE = 30
+FONT_SIZE = 25
 
 
 def main():
     puzzle = Puzzle(PUZZLE_HEIGHT, PUZZLE_WIDTH, ROW_HINTS, COL_HINTS)
     nonogram = Nonogram(puzzle)
-    ui = Ui(nonogram, CELL_SIZE)
+    ui = Ui(nonogram, CELL_SIZE, FONT_SIZE)
     ui.run()
 
 
--- src/ui.py
+++ src/ui.py
@@ -7,20 +7,25 @@ COLOUR_UNKNOWN = (130, 133, 130)
 COLOUR_FULL = (56, 53, 53)
 COLOUR_EMPTY = (210, 213, 210)
 COLOUR_MAYBE = (220, 30, 170)
+COLOUR_HINT_BACKGROUND = (80, 83, 80)
+COLOUR_HINT_FONT = (255, 255, 255)
 WIDTH_BORDER = 1
 GAME_FPS = 60
 
 
 class Ui:
-    def __init__(self, nonogram: Nonogram, cell_size: int):
+    def __init__(self, nonogram: Nonogram, cell_size: int, font_size: int):
         pygame.init()
         pygame.display.set_caption("fx-nono")
         self.nonogram = nonogram
         self.cell_size: int = cell_size
+        self.font = pygame.font.SysFont("Consolas", font_size)
         self.clock = pygame.time.Clock()
 
-        width = nonogram.puzzle.columns * self.cell_size
-        height = nonogram.puzzle.rows * self.cell_size
+        self.max_row_hints = max(list(map(len, nonogram.puzzle.row_hints)))
+        self.max_col_hints = max(list(map(len, nonogram.puzzle.column_hints)))
+        width = (nonogram.puzzle.columns + self.max_row_hints) * self.cell_size
+        height = (nonogram.puzzle.rows + self.max_col_hints) * self.cell_size
         self.screen = pygame.display.set_mode((width, height))
 
         self.running = True
@@ -33,6 +38,7 @@ class Ui:
                     self.running = False
                 elif event.type == pygame.MOUSEBUTTONUP:
                     self.__handle_click(event.button, event.pos)
+            self.draw_hints()
             self.draw_grid()
             pygame.display.flip()
             _ = self.clock.tick(GAME_FPS)
@@ -59,8 +65,8 @@ class Ui:
 
     def __cell_coord_from_position(self, position):
         x, y = position
-        col = x // self.cell_size
-        row = y // self.cell_size
+        col = x // self.cell_size - self.max_row_hints
+        row = y // self.cell_size - self.max_col_hints
         col = max(0, col)
         row = max(0, row)
         col = min(col, self.nonogram.grid.columns - 1)
@@ -69,23 +75,60 @@ class Ui:
 
     def draw_grid(self):
         grid = self.nonogram.grid
-        for x in range(grid.columns):
-            for y in range(grid.rows):
+        for col in range(grid.columns):
+            for row in range(grid.rows):
                 colour = ()
-                if grid.is_cell_full(x, y):
+                if grid.is_cell_full(col, row):
                     colour = COLOUR_FULL
-                elif grid.is_cell_empty(x, y):
+                elif grid.is_cell_empty(col, row):
                     colour = COLOUR_EMPTY
-                elif grid.is_cell_maybe(x, y):
+                elif grid.is_cell_maybe(col, row):
                     colour = COLOUR_MAYBE
                 else:
                     colour = COLOUR_UNKNOWN
-                pygame.draw.rect(self.screen, colour, self.__grid_rect(x, y))
+                pygame.draw.rect(
+                    self.screen,
+                    colour,
+                    self.__grid_rect(
+                        self.max_row_hints + col, self.max_col_hints + row
+                    ),
+                )
 
-    def __grid_rect(self, x: int, y: int):
+    def __grid_rect(self, col: int, row: int):
         return (
-            x * self.cell_size + WIDTH_BORDER,
-            y * self.cell_size + WIDTH_BORDER,
+            col * self.cell_size + WIDTH_BORDER,
+            row * self.cell_size + WIDTH_BORDER,
             self.cell_size - 2 * WIDTH_BORDER,
             self.cell_size - 2 * WIDTH_BORDER,
         )
+
+    def draw_hints(self):
+        for i in range(self.nonogram.grid.columns):
+            for j in range(self.max_col_hints):
+                rect = pygame.draw.rect(
+                    self.screen,
+                    COLOUR_HINT_BACKGROUND,
+                    self.__grid_rect(self.max_row_hints + i, j),
+                )
+                column_hints = self.nonogram.puzzle.column_hints[i]
+                no_of_empty_hint_cells = self.max_col_hints - len(column_hints)
+                if j - no_of_empty_hint_cells >= 0:
+                    hint_text = str(column_hints[j - no_of_empty_hint_cells])
+                    text_surface = self.font.render(hint_text, True, COLOUR_HINT_FONT)
+                    rect_alignment = text_surface.get_rect(center=rect.center)
+                    self.screen.blit(text_surface, rect_alignment)
+
+        for i in range(self.nonogram.grid.rows):
+            for j in range(self.max_row_hints):
+                rect = pygame.draw.rect(
+                    self.screen,
+                    COLOUR_HINT_BACKGROUND,
+                    self.__grid_rect(j, self.max_col_hints + i),
+                )
+                row_hints = self.nonogram.puzzle.row_hints[i]
+                no_of_empty_hint_cells = self.max_row_hints - len(row_hints)
+                if j - no_of_empty_hint_cells >= 0:
+                    hint_text = str(row_hints[j - no_of_empty_hint_cells])
+                    text_surface = self.font.render(hint_text, True, COLOUR_HINT_FONT)
+                    rect_alignment = text_surface.get_rect(center=rect.center)
+                    self.screen.blit(text_surface, rect_alignment)
